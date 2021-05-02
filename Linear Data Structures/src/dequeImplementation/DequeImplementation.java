74aaa542a5056d9870f95e2d2641f89b8bc95a59
Rusev11 <ivaylo.rusev@outlook.com>
2021-05-02 15:58:00 +0300

    removeFIrst method implemeted

--- Linear Data Structures/src/dequeImplementation/DequeImplementation.java
+++ Linear Data Structures/src/dequeImplementation/DequeImplementation.java
@@ -17,20 +17,43 @@ public class DequeImplementation<T> extends DynamicStack {
         private T element;
         private Node next;
         private Node prev;
+        private boolean addOrder;
 
 
         public Node(T element) {
             this.element = element;
             this.next = null;
             this.prev = null;
+            this.addOrder = false;
         }
     }
 
-    public void addFirst(T object){
+    public void addFirst(T object) {
         Node currentNode = new Node(object);
+        currentNode.addOrder = true;
+        if (head == null) {
+            head = currentNode;
+            return;
+        }
         currentNode.next = head;
         head = currentNode;
+    }
 
+    public void removeFirst(T object) {
+        if (head == null) {
+            System.out.println("The list is empty. Nothing to remove.");
+            return;
+        }
+
+        if (!head.addOrder) {
+            System.out.println("This node was added last. Cannot be removed using removeFirst method.");
+            return;
+        }
+        if (head.next == null) {
+            head = null;
+            return;
+        }
+        head = head.next;
     }
 
 }
